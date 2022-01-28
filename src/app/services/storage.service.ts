c031ce8baa71772c197c5e4a8ab8704721975e82
brassSyntax <leon.karpicz@gmail.com>
2022-01-28 23:15:40 +0200

    add @ionic/storage-angular for persistent storage

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,41 @@
+import {Injectable} from '@angular/core';
+import {Storage} from "@ionic/storage-angular";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+  private _storage: Storage | null = null;
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  async init() {
+    this._storage = await this.storage.create();
+  }
+
+  public set(key: string, value: any) {
+    this._storage?.set(key, value);
+  }
+
+  public get(key: string) {
+    return this._storage?.get(key);
+  }
+
+  public remove(key: string) {
+    this._storage?.remove(key);
+  }
+
+  public clear() {
+    this._storage?.clear();
+  }
+
+  public keys() {
+    return this._storage?.keys();
+  }
+
+  public length() {
+    return this._storage?.length();
+  }
+}
